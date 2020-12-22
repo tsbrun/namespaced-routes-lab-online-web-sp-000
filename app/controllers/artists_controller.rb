7e97e64487895b0315ebdae7e736cee0a1fe5a3d
TSephanya Brun <tsbrun@icloud.com>
2020-12-22 14:16:01 -0500

    commit tree 03018dd3bfee427f00f8e69e69292ce9c4982e28

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    p = Preference.create
+    if p.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
